310175b2f8068026349183f1dad5112fbdec67f0
masha <92234623+sofiamasha@users.noreply.github.com>
2025-05-07 14:43:46 -0300

    exemplo01.js

---
+++ exemplo01.js
@@ -0,0 +1,23 @@
+function lerEntrada() {
+
+    document.getElementById("resp").innerHTML = "";
+
+
+    var idade = document.getElementById("idade").value;
+
+ 
+    if (idade != "" && idade != undefined && idade != null) {
+        document.getElementById("resp").innerHTML = `<div class='tile'>Idade informada: ${idade} </div>`;
+        votacao(idade);
+    }
+}
+
+function votacao(idade) {
+    if (idade < 18) {
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é menor de idade.</div>";
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa não pode votar.</div>";
+    }
+    else { }
+    document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é maior de idade.</div>";
+    document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa pode votar.</div>";
+}
